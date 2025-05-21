11a9ef25e9444426f8e2ba5f6254bee752e8fc69
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-05-21 13:24:47 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -11,21 +11,20 @@ typedef struct {
     size_t     count;
 } FileSystem;
 
-/* загрузка / сохранение / освобождение */
+
 int  fs_load (const char *fname, FileSystem *fs);
 int  fs_save (const char *fname, const FileSystem *fs);
 void fs_free (FileSystem *fs);
 
-/* CRUD */
+
 int  fs_select(const FileSystem *fs, const char *path, char **out_content);
 int  fs_insert(FileSystem *fs, const char *path);
 int  fs_update(FileSystem *fs, const char *path, const char *new_content);
 int  fs_delete(FileSystem *fs, const char *path);
 
-/* новые функции */
 int    fs_crypto (FileSystem *fs, const char *path, const char *key);
 int    fs_decrypto(FileSystem *fs, const char *path, const char *key);
 size_t fs_count  (const FileSystem *fs);
 int    fs_rename (FileSystem *fs, const char *old_path, const char *new_path);
 
-#endif /* FILESYSTEM_H */
+#endif 
